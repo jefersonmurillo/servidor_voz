b11d8bc211e790bbbfccecbd97779567d3303fc5
jmurilloariza <jmurilloariza@gmail.com>
2018-04-03 15:37:49 -0500

    primer commit

---
+++ servidor.js
@@ -0,0 +1,48 @@
+var servidor=function(puerto,dir){
+  //paquete encargado de cargar un archivo del disco duro
+  fs=require("fs");
+  
+  var opciones={
+    key:fs.readFileSync("./certificado/privatekey.pem"),
+    cert:fs.readFileSync("./certificado/certificate.pem")
+  }
+  //inicilizar variable https como un servidor
+  http = require('https').createServer(opciones,crearServidor);
+  //paquete encargado del uso relativo de request
+  path=require("path");
+  //creacion del servidor en un puerto y url espificada por la ip de la maquina donde se este ejecutando el servidor web
+  http.listen(puerto,dir,()=>{
+    console.log("servidor web ejecutandose  en la direccion ","https://"+dir+":"+puerto);
+  });
+  //vector creado para controlar a que archivos del servidor pueden acceder los usuarios
+ var url=[
+    {
+      ruta:'',
+      salida:'/pagina.html',
+      type:'text/html'
+    },
+    {
+       ruta:'/p',
+      salida:'/prueba1.html',
+      type:'text/html'
+    }
+  ];
+  //funcion encargada de recibir todas las request y enviar los response a los usuarios
+    function crearServidor (req, res) {
+      //variable encarga de parsear la url entrante para un tratamiento mas simple
+      var pathUrl=path.basename(req.url);
+      //head de respuesta inicial
+      res.writeHead(404,{'Content-Type': 'text/html'});
+      //ciclo encargado de buscar en el vector url si el archivo al que quiere acceder el usuario es correcto o tiene permiso
+      url.forEach(function (pos){
+        //condicion encargada de validar si esa url existe
+        if(pos.ruta.includes(pathUrl)){
+          //head final
+          res.writeHead(200, {'Content-Type': pos.type});
+          //envio de archivo al usuario
+          res.end(fs.readFileSync(__dirname+pos.salida));
+        }
+      })
+  }
+}
+exports.iniciar=servidor;
